a19d0a14cc4b93e9361fe037e0b9c4c0288c4592
tufail <786tufail@gmail.com>
2022-10-19 10:48:26 +0530

    ci(topbar): device swicther ui

--- src/editorComponents/TopBar/index.js
+++ src/editorComponents/TopBar/index.js
@@ -1,9 +1,21 @@
 import React from 'react';
 
+import DesktopIcon from '../../icons/DeskTopIcon';
+
 const TopBar = ({ deviceSwitcherHanlder }) => {
   return (
-    <div className="be__topbar" onClick={() => deviceSwitcherHanlder('md')}>
-      top bar
+    <div className="be__topbar">
+      <div className="be__topbar__toolbar">
+        <span onClick={() => deviceSwitcherHanlder('lg')}>
+          <DesktopIcon />
+        </span>
+        <span onClick={() => deviceSwitcherHanlder('md')}>
+          <DesktopIcon />
+        </span>
+        <span onClick={() => deviceSwitcherHanlder('sm')}>
+          <DesktopIcon />
+        </span>
+      </div>
     </div>
   );
 };
